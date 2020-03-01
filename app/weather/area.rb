bca40012e4587425ab7fe63dee093b2c7e40d87b
onikunai <greentea5461@gmail.com>
2020-03-01 16:00:47 +0900

    Update area.rb

--- app/weather/area.rb
+++ app/weather/area.rb
@@ -324,7 +324,7 @@ class Weather_area
       text1 = "神戸"
       text2 = "豊岡"
       text2 = "次へ"
-      template = form.template(title, text1, text2)
+      template = form.template(title, text1, text2, pref)
     else
       pref = "都道府県を送信して下さい。\n記入例：北海道、東京都、兵庫県など"
       message = {
