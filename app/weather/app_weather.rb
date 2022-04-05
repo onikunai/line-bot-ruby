5190fde583e57bbaa8cd4f747a56c641222de37f
onikunai <o66.okubo.shinya@gmail.com>
2022-04-05 09:26:21 +0900

    Update app_weather.rb

--- app/weather/app_weather.rb
+++ app/weather/app_weather.rb
@@ -34,8 +34,8 @@ class Weather
     # url = YAML.load_file('./config/config.yml')["weather-url"]
     # url = "http://weather.livedoor.com/forecast/webservice/json/v1"
     # url = "https://weather.tsukumijima.net/api/forecast"
-    #url = "https://weather.tsukumijima.net/api/forecast/city/"
-    url = "https://weather.tsukumijima.net/api/forecast?city="
+    url = "https://weather.tsukumijima.net/api/forecast/city"
+    # url = "https://weather.tsukumijima.net/api/forecast?city="
     return analysisWeather(connectionAPI(keyWord, url))
   end
 
